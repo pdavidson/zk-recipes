fc0dba0502559dee4936d283443cc4c8054305ad
Peter Davidson <Peter.Davidson@BodyBuilding.com>
2016-09-16 10:22:56 -0600

    Initial Commit

---
+++ hello.rb
@@ -0,0 +1,6 @@
+zookeeper '3.4.8' do
+  user     'zookeeper'
+  mirror   'http://www.poolsaboveground.com/apache/zookeeper'
+  checksum 'f10a0b51f45c4f64c1fe69ef713abf9eb9571bc7385a82da892e83bb6c965e90'
+  action   :install
+end
